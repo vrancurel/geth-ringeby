7cecf313cddb5cd7aab6d04fdfc512eb4f743fb3
vrancurel <vianney.rancurel@gmail.com>
2018-02-09 11:30:02 -0800

    start rpc server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -x
 
-/geth --datadir=/root/rinkeby --rinkeby --syncmode light --cache 2048 --verbosity 3
+geth --datadir=/root/rinkeby --rinkeby --syncmode light --cache 2048 \
+ console --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpcapi eth,net,personal,web3
